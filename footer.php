dfb5b6346d2a0ed8bfe2a8912a1197e90dd990cc
Cheneba <chenebametuge14@gmail.com>
2025-02-25 13:17:05 +0100

    Added the homepage

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
-<footer>Footer</footer>
+<footer>
+  <div class="footer">
+    <ul class="links-footer">
+      <li class="footer-link">
+        <a href="/">Home</a>
+      </li>
+      <li class="footer-link">
+        <a href="/about.php">About</a>
+      </li>
+      <li class="footer-link">
+        <a href="/contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</footer>
 </body>
 
 </html>
