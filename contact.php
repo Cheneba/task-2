8e77dcd26941f30d7e830dab17907623f83150ad
Cheneba <chenebametuge14@gmail.com>
2025-02-25 14:27:05 +0100

    Trying to adjust some basic post request reception

--- contact.php
+++ contact.php
@@ -1,20 +1,39 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $title = htmlspecialchars($_POST['subject']);
+  $message = htmlspecialchars($_POST['message']);
+  var_dump($title);
+  $ErrName = $name ? null : "required";
+  $ErrEmail = $email ? null : "required";
+  $ErrTitle = $title ? null : "required";
+
+  if ($name && $email && $title) {
+    // Code to connect to the database and store this data there
+  }
+}
+
+?>
+
 <?php include "header.php"; ?>
 
 
 <div class="contact-body">
   <div class="container">
-    <form action="#" method="POST">
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
       <div class="part-1">
         <label for="name">Name</label>
-        <input type="text" id="name" placeholder="Name" />
+        <input type="text" id="name" placeholder="Name" required />
 
         <label for="email">Email</label>
-        <input type="text" id="email" placeholder="Email" />
+        <input type="text" id="email" placeholder="Email" required />
       </div>
       <br />
       <div class="part-2">
         <label for="subject">Subject</label>
-        <input type="text" id="subject" placeholder="Subject" />
+        <input type="text" id="subject" placeholder="Subject" required />
         <br />
         <br />
         Message
@@ -22,7 +41,7 @@
       </div>
 
       <div class="submit-btn">
-        <button type="submit">Send</button>
+        <button type="submit" id="submit">Send</button>
       </div>
     </form>
   </div>
