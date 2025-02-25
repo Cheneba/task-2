83a9935a9755165b6d29355245ba79a5820be09e
Cheneba <chenebametuge14@gmail.com>
2025-02-25 13:30:34 +0100

    Added info on the about page

--- about.php
+++ about.php
@@ -1,3 +1,18 @@
 <?php include "header.php"; ?>
-<main></main>
+
+
+<div class="about-body">
+  <div class="container">
+    <h1>Hello</h1>
+    <p>Welcome to my page</p>
+    <h3>Recap</h3>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quis
+      doloribus odit illo possimus! Assumenda libero earum error quibusdam
+      eum, id, dolore placeat consequatur quia excepturi, odit nihil saepe
+      fuga!
+    </p>
+  </div>
+</div>
+
 <?php include "footer.php"; ?>
