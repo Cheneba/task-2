40d7f0564bf4cf566b0c2971d72544ba1e348c87
Cheneba <chenebametuge14@gmail.com>
2025-02-25 13:16:22 +0100

    Added footer and header code into their respective files

--- header.php
+++ header.php
@@ -2,10 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
-    <header>Header</header>
+  <header>
+    <div class="header">
+      <ul class="links-header">
+        <li class="header-link profile"></li>
+        <li class="header-link">
+          <a href="/">Home</a>
+        </li>
+        <li class="header-link">
+          <a href="/about.php">About</a>
+        </li>
+        <li class="header-link">
+          <a href="/contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </header>
