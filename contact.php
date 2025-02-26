b3f870c7b1e0120b82e1d741725ea68df4d7fdc0
Cheneba <chenebametuge14@gmail.com>
2025-02-26 05:12:40 +0100

    Rounding up with the contact page. Implemented the response on http get method with query and adjusted the UI

--- contact.php
+++ contact.php
@@ -1,20 +1,7 @@
 <?php
-
-if (isset($_POST['submit'])) {
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $title = htmlspecialchars($_POST['subject']);
-  $message = htmlspecialchars($_POST['message']);
-  var_dump($title);
-  $ErrName = $name ? null : "required";
-  $ErrEmail = $email ? null : "required";
-  $ErrTitle = $title ? null : "required";
-
-  if ($name && $email && $title) {
-    // Code to connect to the database and store this data there
-  }
+if (isset($_GET['textarea']) || isset($_GET['msg'])) {
+  $msg = htmlspecialchars($_GET['textarea']) ?: htmlspecialchars($_GET['msg']);
 }
-
 ?>
 
 <?php include "header.php"; ?>
@@ -22,26 +9,28 @@ if (isset($_POST['submit'])) {
 
 <div class="contact-body">
   <div class="container">
-    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-      <div class="part-1">
-        <label for="name">Name</label>
-        <input type="text" id="name" placeholder="Name" required />
+    <h1 style="margin-top: 50px;">Contact us</h1>
+    <br>
+    <hr>
+    <br>
+    <p>I am available for whatever request you have. Here are the various ways you can get to me:</p>
+    <br>
+    <p>Email: <strong>chenebametuge14@gmail.com</strong></p>
+    <p>Tel(Whatsapp): <strong>+237 673893817</strong></p>
+    <p>Slack Handle: <strong>@Metuge</strong></p>
 
-        <label for="email">Email</label>
-        <input type="text" id="email" placeholder="Email" required />
-      </div>
-      <br />
-      <div class="part-2">
-        <label for="subject">Subject</label>
-        <input type="text" id="subject" placeholder="Subject" required />
-        <br />
-        <br />
-        Message
-        <textarea name="message" id="message"></textarea>
-      </div>
-
+    <br>
+    <hr>
+    <p>
+      <?php echo $msg ? "Previous Message: $msg" : "No previous message" ?>
+    </p>
+    <hr>
+    <br>
+    <p>Enter a message</p>
+    <form action="/task/contact.php" method="GET">
+      <textarea name="textarea" id="textarea"></textarea>
       <div class="submit-btn">
-        <button type="submit" id="submit">Send</button>
+        <button type="submit">Send</button>
       </div>
     </form>
   </div>
